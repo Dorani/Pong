88b03e8016b87eb98d50ca7b02f8d7283cdca397
Seif <seif.dorani@gmail.com>
2016-11-20 23:16:42 -0500

    functions for both key strokes

--- pong.js
+++ pong.js
@@ -47,3 +47,21 @@ setInterval(draw, 10);
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
