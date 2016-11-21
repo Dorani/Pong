57e2bf1efcc2cb1ba5eee09f7b17dce52ddacafc
Seif <seif.dorani@gmail.com>
2016-11-20 23:21:02 -0500

    paddle moving logic

--- pong.js
+++ pong.js
@@ -31,6 +31,7 @@ function drawPaddle() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -38,6 +39,13 @@ function draw() {
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+}
+else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
+}
+
 
     x += dx;
     y += dy;
